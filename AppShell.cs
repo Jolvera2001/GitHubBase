314327dd953be2473453d6f7bfe48d9401ff96f1
Jolvera2001 <jolvera6@stedwards.edu>
2024-07-05 17:58:39 -0500

    Add project files.

---
+++ AppShell.cs
@@ -0,0 +1,28 @@
+﻿using GitHubBase.Pages;
+using MauiReactor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubBase
+{
+    internal class AppShell : Component
+    {
+        public override VisualNode Render()
+            => Shell(
+                FlyoutItem("MainPage",
+                    ShellContent()
+                        .Title("MainPage")
+                        .RenderContent(() => new MainPage())
+                ),
+                FlyoutItem("OtherPage",
+                    ShellContent()
+                        .Title("OtherPage")
+                        .RenderContent(() => new OtherPage())
+                )
+            );
+    }
+
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using GitHubBase.Pages;
+using MauiReactor;
+using Microsoft.Extensions.Logging;
+
+
+namespace GitHubBase
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiReactorApp<AppShell>(app =>
+                {
+                    app.AddResource("Resources/Styles/Colors.xaml");
+                    app.AddResource("Resources/Styles/Styles.xaml");
+                })
+#if DEBUG
+                .EnableMauiReactorHotReload()
+#endif
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-SemiBold.ttf", "OpenSansSemiBold");
+                });
+
+#if DEBUG
+        		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
