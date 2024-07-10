e1acacdca42b9d69ac099da1e765473cda607c2b
Jolvera2001 <jolvera6@stedwards.edu>
2024-07-09 21:18:50 -0500

    added in explicit key annotation just cause

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace GitHubBase.Domain.Entities;
 
 public class User
 {
+    [Key]
     public int Id { get; set; }
     public string? AccountNickname { get; set; }
     public string? GitHubUsername { get; set; }
